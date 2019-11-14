d81ea7e2cf7c89a75524db0046d3ecc3151590f6
Apirat Chiwakanit <apirat.c@kbtg.tech>
2019-11-14 08:51:34 +0700

    development env

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
 				<img src={logo} className="App-logo" alt="logo" />
 				<p>REACT_APP_PROJECT_NAME : {process.env.REACT_APP_PROJECT_NAME}</p>
 				<p>NODE_ENV : {process.env.NODE_ENV}</p>
+				<p>REACT_APP_API_END_POINT : {process.env.REACT_APP_API_END_POINT}</p>
 			</header>
 		</div>
 	)
