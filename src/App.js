00369b045a8eb59068805d09dfdc44ab2b102a54
Apirat Chiwakanit <apirat.c@kbtg.tech>
2019-11-14 08:35:59 +0700

    create .env file and showing the process env variable

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ function App() {
 		<div className="App">
 			<header className="App-header">
 				<img src={logo} className="App-logo" alt="logo" />
+				<p>REACT_APP_PROJECT_NAME : {process.env.REACT_APP_PROJECT_NAME}</p>
+				<p>NODE_ENV : {process.env.NODE_ENV}</p>
 			</header>
 		</div>
 	)
