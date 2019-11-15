12f087487e93c33ee60a561a0c43fe810ea2e7f8
Apirat Chiwakanit <apirat.c@kbtg.tech>
2019-11-15 14:04:34 +0700

    Home page

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,21 @@
+import React from "react"
+import { Link } from "react-router-dom";
+import logo from "../../assets/logo/logo.svg"
+import "./Home.css"
+
+export default function Home() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+
+                <Link to="/" className="btn btn-info">Home</Link>
+
+                <p>REACT_APP_PROJECT_NAME : {process.env.REACT_APP_PROJECT_NAME}</p>
+                <p>NODE_ENV : {process.env.NODE_ENV}</p>
+                <p>REACT_APP_ENV_NAME : {process.env.REACT_APP_ENV_NAME}</p>
+                <p>REACT_APP_API_END_POINT : {process.env.REACT_APP_API_END_POINT}</p>
+            </header>
+        </div>
+    )
+}
