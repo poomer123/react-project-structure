19829a20fbcfeb35f9e3bd3e5822970edcb88f72
Apirat Chiwakanit <apirat.c@kbtg.tech>
2019-11-15 11:32:31 +0700

    use router

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react"
+import { Link } from "react-router-dom";
 import logo from "./logo.svg"
 import "./App.css"
 
@@ -7,6 +8,9 @@ function App() {
 		<div className="App">
 			<header className="App-header">
 				<img src={logo} className="App-logo" alt="logo" />
+
+				<Link to="/" className="btn btn-info">Home</Link>
+
 				<p>REACT_APP_PROJECT_NAME : {process.env.REACT_APP_PROJECT_NAME}</p>
 				<p>NODE_ENV : {process.env.NODE_ENV}</p>
 				<p>REACT_APP_ENV_NAME : {process.env.REACT_APP_ENV_NAME}</p>
