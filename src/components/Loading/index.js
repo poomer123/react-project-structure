5ebd30bffb581a634046625ca3b357746aeb55e3
Apirat Chiwakanit <apirat.c@kbtg.tech>
2019-11-18 11:07:35 +0700

    loading component

---
+++ src/components/Loading/index.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+function Loading(props) {
+    return (
+        <div className="spinner-border m-5" role="status">
+            <span className="sr-only">Loading..</span>
+        </div>
+    )
+}
+
+export default Loading
