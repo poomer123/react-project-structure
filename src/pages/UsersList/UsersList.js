610c70747e41537299b5f1fbfd19b0f43dc62673
Apirat Chiwakanit <apirat.c@kbtg.tech>
2019-11-18 12:35:11 +0700

    use container

--- src/pages/UsersList/UsersList.js
+++ src/pages/UsersList/UsersList.js
@@ -1,45 +1,12 @@
-import React, { useEffect, useState } from "react"
+import React from "react"
 import MainLayout from "../../layouts/Main";
-import Loading from "../../components/Loading";
-import { mainService } from "../../services";
+import DataListingContainer from "../../common/DataListingContainer";
 
 function UsersList(props) {
 
-    const [usersData, setUsersData] = useState([])
-    const [isLoading, setIsLoading] = useState(true)
-
-    useEffect(() => {
-        mainService.getUsers(
-            'https://jsonplaceholder.typicode.com/users',
-            onSuccess
-        )
-    }, [])
-
-    const onSuccess = (dataToSet = [], loadingToSet = true) => {
-        setUsersData(dataToSet)
-        setIsLoading(loadingToSet)
-    }
-
     return (
         <MainLayout>
-            <div className="row">
-                <div className="col">
-                    <h1>Users List</h1>
-                    {isLoading
-                        ? <Loading />
-                        : <>
-                            {usersData.length > 0
-                                ? <ul>
-                                    {usersData.map(
-                                        user => <li key={user.id}>{user.name}</li>
-                                    )}
-                                </ul>
-                                : <h3>No data..</h3>
-                            }
-                        </>
-                    }
-                </div>
-            </div>
+            <DataListingContainer title={'Users Listing Page'} url={'https://jsonplaceholder.typicode.com/users'} />
         </MainLayout>
     )
 }
