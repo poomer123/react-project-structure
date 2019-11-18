9f50807d879d0e0a766ec42d5aa80929c021ee72
Apirat Chiwakanit <apirat.c@kbtg.tech>
2019-11-18 14:08:05 +0700

    reuse component for users page

--- src/pages/Users/Users.js
+++ src/pages/Users/Users.js
@@ -1,16 +1,11 @@
 import React from "react"
 import MainLayout from "../../layouts/Main";
+import DataListingContainer from "../../common/DataListingContainer";
 
 export default function Users() {
     return (
         <MainLayout>
-            <div className="container py-5">
-                <div className="row">
-                    <div className="col">
-                        <h1>User</h1>
-                    </div>
-                </div>
-            </div>
+            <DataListingContainer title={'Users Page'} url={'https://jsonplaceholder.typicode.com/users'} />
         </MainLayout>
     )
 }
